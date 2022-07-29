4a421e18d6e64758f1ac3de0860e7612068bedd0
emir.pajic <emir.pajic@mss.ba>
2022-07-29 21:40:36 +0200

    Added styles for country list

--- eDentist.WinUI/Forms/Countries/CountryList.Designer.cs
+++ eDentist.WinUI/Forms/Countries/CountryList.Designer.cs
@@ -30,6 +30,10 @@ namespace eDentist.WinUI.Forms.Countries
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CountryList));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.App = new System.Windows.Forms.Label();
             this.btnAddCountry = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -43,57 +47,61 @@ namespace eDentist.WinUI.Forms.Countries
             // App
             // 
             this.App.AutoSize = true;
-            this.App.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.App.Location = new System.Drawing.Point(28, 58);
+            this.App.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.App.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.App.Location = new System.Drawing.Point(91, 78);
             this.App.Name = "App";
-            this.App.Size = new System.Drawing.Size(86, 20);
+            this.App.Size = new System.Drawing.Size(220, 29);
             this.App.TabIndex = 70;
-            this.App.Text = "Countries";
+            this.App.Text = "Manage countries";
             // 
             // btnAddCountry
             // 
+            this.btnAddCountry.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnAddCountry.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAddCountry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAddCountry.Image = ((System.Drawing.Image)(resources.GetObject("btnAddCountry.Image")));
-            this.btnAddCountry.Location = new System.Drawing.Point(454, 95);
+            this.btnAddCountry.Location = new System.Drawing.Point(476, 56);
             this.btnAddCountry.Name = "btnAddCountry";
-            this.btnAddCountry.Size = new System.Drawing.Size(41, 37);
+            this.btnAddCountry.Size = new System.Drawing.Size(61, 51);
             this.btnAddCountry.TabIndex = 69;
-            this.btnAddCountry.UseVisualStyleBackColor = true;
+            this.btnAddCountry.UseVisualStyleBackColor = false;
             this.btnAddCountry.Click += new System.EventHandler(this.btnAddCountry_Click);
             // 
             // btnDelete
             // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
-            this.btnDelete.Location = new System.Drawing.Point(573, 95);
+            this.btnDelete.Location = new System.Drawing.Point(632, 56);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(41, 37);
+            this.btnDelete.Size = new System.Drawing.Size(61, 51);
             this.btnDelete.TabIndex = 67;
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnCountryDetails
             // 
+            this.btnCountryDetails.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnCountryDetails.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCountryDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCountryDetails.Image = ((System.Drawing.Image)(resources.GetObject("btnCountryDetails.Image")));
-            this.btnCountryDetails.Location = new System.Drawing.Point(514, 95);
+            this.btnCountryDetails.Location = new System.Drawing.Point(553, 56);
             this.btnCountryDetails.Name = "btnCountryDetails";
-            this.btnCountryDetails.Size = new System.Drawing.Size(41, 37);
+            this.btnCountryDetails.Size = new System.Drawing.Size(61, 51);
             this.btnCountryDetails.TabIndex = 66;
-            this.btnCountryDetails.UseVisualStyleBackColor = true;
+            this.btnCountryDetails.UseVisualStyleBackColor = false;
             this.btnCountryDetails.Click += new System.EventHandler(this.btnCountryDetails_Click);
             // 
             // txtSearch
             // 
-            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtSearch.Location = new System.Drawing.Point(28, 108);
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtSearch.Location = new System.Drawing.Point(92, 160);
             this.txtSearch.Multiline = true;
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(275, 24);
+            this.txtSearch.Size = new System.Drawing.Size(322, 32);
             this.txtSearch.TabIndex = 65;
             this.txtSearch.Text = "Enter Name";
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
@@ -102,14 +110,48 @@ namespace eDentist.WinUI.Forms.Countries
             // 
             this.dvgCountries.AllowUserToAddRows = false;
             this.dvgCountries.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(239)))), ((int)(((byte)(249)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dvgCountries.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dvgCountries.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.dvgCountries.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dvgCountries.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dvgCountries.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dvgCountries.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dvgCountries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dvgCountries.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.CountryName});
-            this.dvgCountries.Location = new System.Drawing.Point(32, 158);
+            this.dvgCountries.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dvgCountries.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dvgCountries.Location = new System.Drawing.Point(96, 210);
             this.dvgCountries.Name = "dvgCountries";
             this.dvgCountries.ReadOnly = true;
+            this.dvgCountries.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dvgCountries.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dvgCountries.RowHeadersVisible = false;
-            this.dvgCountries.Size = new System.Drawing.Size(216, 150);
+            this.dvgCountries.Size = new System.Drawing.Size(597, 244);
             this.dvgCountries.TabIndex = 71;
             // 
             // CountryName
@@ -124,6 +166,7 @@ namespace eDentist.WinUI.Forms.Countries
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.dvgCountries);
             this.Controls.Add(this.App);
             this.Controls.Add(this.btnAddCountry);
@@ -131,7 +174,7 @@ namespace eDentist.WinUI.Forms.Countries
             this.Controls.Add(this.btnCountryDetails);
             this.Controls.Add(this.txtSearch);
             this.Name = "CountryList";
-            this.Size = new System.Drawing.Size(642, 376);
+            this.Size = new System.Drawing.Size(720, 561);
             this.Load += new System.EventHandler(this.CountriesList_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dvgCountries)).EndInit();
             this.ResumeLayout(false);
