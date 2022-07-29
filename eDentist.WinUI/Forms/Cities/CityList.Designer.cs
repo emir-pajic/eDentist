172050e1f2bdb6216763a30593940d7f9bd964c7
emir.pajic <emir.pajic@mss.ba>
2022-07-29 21:30:07 +0200

    Added styles for city list

--- eDentist.WinUI/Forms/Cities/CityList.Designer.cs
+++ eDentist.WinUI/Forms/Cities/CityList.Designer.cs
@@ -32,6 +32,8 @@ namespace eDentist.WinUI.Forms.Cities
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CityList));
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.App = new System.Windows.Forms.Label();
             this.btnAddCity = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -47,57 +49,61 @@ namespace eDentist.WinUI.Forms.Cities
             // App
             // 
             this.App.AutoSize = true;
-            this.App.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.App.Location = new System.Drawing.Point(37, 47);
+            this.App.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.App.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.App.Location = new System.Drawing.Point(108, 67);
             this.App.Name = "App";
-            this.App.Size = new System.Drawing.Size(54, 20);
+            this.App.Size = new System.Drawing.Size(175, 29);
             this.App.TabIndex = 77;
-            this.App.Text = "Cities";
+            this.App.Text = "Manage cities";
             // 
             // btnAddCity
             // 
+            this.btnAddCity.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnAddCity.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAddCity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAddCity.Image = ((System.Drawing.Image)(resources.GetObject("btnAddCity.Image")));
-            this.btnAddCity.Location = new System.Drawing.Point(463, 84);
+            this.btnAddCity.Location = new System.Drawing.Point(492, 47);
             this.btnAddCity.Name = "btnAddCity";
-            this.btnAddCity.Size = new System.Drawing.Size(41, 37);
+            this.btnAddCity.Size = new System.Drawing.Size(55, 49);
             this.btnAddCity.TabIndex = 76;
-            this.btnAddCity.UseVisualStyleBackColor = true;
+            this.btnAddCity.UseVisualStyleBackColor = false;
             this.btnAddCity.Click += new System.EventHandler(this.btnAddCity_Click);
             // 
             // btnDelete
             // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
-            this.btnDelete.Location = new System.Drawing.Point(582, 84);
+            this.btnDelete.Location = new System.Drawing.Point(647, 47);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(41, 37);
+            this.btnDelete.Size = new System.Drawing.Size(55, 49);
             this.btnDelete.TabIndex = 74;
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnDetails
             // 
+            this.btnDetails.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnDetails.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDetails.Image = ((System.Drawing.Image)(resources.GetObject("btnDetails.Image")));
-            this.btnDetails.Location = new System.Drawing.Point(523, 84);
+            this.btnDetails.Location = new System.Drawing.Point(574, 47);
             this.btnDetails.Name = "btnDetails";
-            this.btnDetails.Size = new System.Drawing.Size(41, 37);
+            this.btnDetails.Size = new System.Drawing.Size(55, 49);
             this.btnDetails.TabIndex = 73;
-            this.btnDetails.UseVisualStyleBackColor = true;
+            this.btnDetails.UseVisualStyleBackColor = false;
             this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
             // 
             // txtSearch
             // 
-            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.txtSearch.Location = new System.Drawing.Point(37, 97);
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtSearch.Location = new System.Drawing.Point(113, 149);
             this.txtSearch.Multiline = true;
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(275, 24);
+            this.txtSearch.Size = new System.Drawing.Size(275, 30);
             this.txtSearch.TabIndex = 72;
             this.txtSearch.Text = "Enter Name";
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
@@ -113,24 +119,43 @@ namespace eDentist.WinUI.Forms.Cities
             this.dgvCities.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvCities.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
             this.dgvCities.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCities.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvCities.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCities.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.CityID,
             this.CityName,
             this.CountryName});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvCities.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvCities.Location = new System.Drawing.Point(41, 138);
+            this.dgvCities.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCities.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvCities.Location = new System.Drawing.Point(113, 203);
             this.dgvCities.Name = "dgvCities";
             this.dgvCities.ReadOnly = true;
+            this.dgvCities.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCities.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dgvCities.RowHeadersVisible = false;
-            this.dgvCities.Size = new System.Drawing.Size(415, 263);
+            this.dgvCities.Size = new System.Drawing.Size(589, 298);
             this.dgvCities.TabIndex = 78;
             // 
             // CityID
@@ -161,6 +186,7 @@ namespace eDentist.WinUI.Forms.Cities
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.dgvCities);
             this.Controls.Add(this.App);
             this.Controls.Add(this.btnAddCity);
@@ -168,7 +194,7 @@ namespace eDentist.WinUI.Forms.Cities
             this.Controls.Add(this.btnDetails);
             this.Controls.Add(this.txtSearch);
             this.Name = "CityList";
-            this.Size = new System.Drawing.Size(672, 442);
+            this.Size = new System.Drawing.Size(800, 621);
             this.Load += new System.EventHandler(this.CitiesList_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCities)).EndInit();
             this.ResumeLayout(false);
