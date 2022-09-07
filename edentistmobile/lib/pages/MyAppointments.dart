c86caebdfa6811bfacfdbd9e3feea19a129c320a
emir.pajic <emir.pajic@mss.ba>
2022-09-07 22:09:13 +0200

    Returned and showed list of users appointments

--- edentistmobile/lib/pages/MyAppointments.dart
+++ edentistmobile/lib/pages/MyAppointments.dart
@@ -5,37 +5,73 @@ import 'package:edentistmobile/services/APIService.dart';
 class MyAppointments extends StatefulWidget {
   @override
   _MyAppointmentsState createState() => _MyAppointmentsState();
-
-
 }
 
 class _MyAppointmentsState extends State<MyAppointments> {
-
-  var welcomeName = "${APIService.signedInUser!.firstName!} ${APIService.signedInUser!.lastname!} appointments";
-
-
-
-
+  var welcomeName =
+      "${APIService.signedInUser!.firstName!} ${APIService.signedInUser!.lastname!} appointments";
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/bg.png'),
-              fit: BoxFit.cover)),
+              image: AssetImage('assets/bg.png'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           title: Text(welcomeName),
           backgroundColor: Colors.blue[900],
+
         ),
-
-
+        body: bodyWidget(),
       ),
     );
   }
 
+  Widget bodyWidget() {
+    return FutureBuilder<List<Appointment>>(
+      future: getMyAppointments(APIService.signedInUser?.userId ?? 0),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<Appointment>> snapshot) {
+            if (snapshot.connectionState==ConnectionState.waiting){
+              return Center(child: Text("Loading...."),);
+            }else if (snapshot.hasError){
+              return Center(child: Text("Error...."),);
 
+            }
+            else{
+              return ListView(
+                  //children: snapshot.data.map((e) => AppointmentWidget(e)),
+                children: snapshot.data!.map((e) => AppointmentWidget(e)).toList(),
+              );
+            }
+          },
+    );
+  }
+
+  Future<List<Appointment>> getMyAppointments(int userId) async{
+    var appointments = await APIService.getmyappointments("Appointments", userId);
+    return appointments;
+
+  }
+
+  Widget AppointmentWidget(appointment){
+
+    // get doctor's ID (accBy) and write name and lastname
+    return Padding(
+      padding: EdgeInsets.fromLTRB(2, 2, 2, 2),
+      child: Card(
+        child: TextButton(
+          onPressed: (){
+            
+          },
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text("Date: " + appointment.date + " | " + appointment.appointmentStatus )),
+        ),
+      ),
+    );
+  }
 
 }
--- edentistmobile/lib/services/APIService.dart
+++ edentistmobile/lib/services/APIService.dart
@@ -73,7 +73,7 @@ class APIService {
         }
       });
 
-      return myappointments;
+      return appointments.toList();
     }
     return null;
   }
