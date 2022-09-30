2c8d29d74bb69941240a499f24ebcee146e8de18
emir.pajic <emir.pajic@mss.ba>
2022-09-30 13:41:41 +0200

    marking examination as paid

--- edentistmobile/lib/pages/ExaminationDetails.dart
+++ edentistmobile/lib/pages/ExaminationDetails.dart
@@ -1,4 +1,4 @@
-
+import 'dart:convert';
 
 import 'package:edentistmobile/controller/payment_controller.dart';
 import 'package:edentistmobile/services/APIService.dart';
@@ -10,12 +10,10 @@ import '../models/Examination.dart';
 class ExaminationDetails extends StatelessWidget {
   final Examination? examination;
 
-
   ExaminationDetails({Key? key, required this.examination}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final paymentController = Get.put(PaymentController());
 
     return Scaffold(
@@ -23,7 +21,6 @@ class ExaminationDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text("Examination Details"),
         backgroundColor: Colors.blue[900],
-
       ),
       body: Card(
         child: Column(
@@ -31,23 +28,45 @@ class ExaminationDetails extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.face),
               title: Text("Examination: ${examination?.additionalInfo}"),
-              subtitle: Text("Treatment: ${examination?.treatmentDesription}\nStatus: ${examination?.status}"),
+              subtitle: Text(
+                  "Treatment: ${examination?.treatmentDesription}\nStatus: ${examination?.status}"),
               trailing: Text("${examination?.price!.toInt().toString()} €"),
-
             ),
             Center(
               child: ElevatedButton(
-                child: Text("Make Payment"),
-                onPressed: () => paymentController.makePayment(amount: '${examination?.price!.toInt()}', currency: 'EUR'),
-              ),
+                  child: Text("Make Payment"),
+                  onPressed: () async {
+                    if (examination?.paymentTokenId != 'payment') {
+                      paymentController.makePayment(
+                          amount: '${examination?.price!.toInt()}',
+                          currency: 'EUR');
+                      Map data = {
+                        'examinationId': examination?.examinationId,
+                        'additionalInfo': examination?.additionalInfo,
+                        'appointmentId': examination?.appointmentId,
+                        'userId': examination?.userId,
+                        'treatmentId': examination?.treatmentId,
+                        'status': examination?.status,
+                        'paymentTokenId': 'payment'
+                      };
+
+                      var body = jsonEncode(data);
+
+                      await APIService.updateExamination(
+                          "Examinations/", examination?.examinationId, body);
+                    } else {
+                      var error = 'This examination is already paid';
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: SizedBox(
+                            height: 40, child: Center(child: Text(error))),
+                        backgroundColor: Colors.orange,
+                      ));
+                    }
+                  }),
             )
           ],
         ),
       ),
     );
   }
-
 }
-
-
-
